ae19f707de9c9ea3abacd8676d07f21492b996b5
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-11 01:38:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,18 @@
 
-def fizzbuzz (num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 5 == 0
-    "Buzz"
-  elsif num % 3 == 0
-    "Fizz"
-  end
-end 
+def  fizzbuzz
+  num = 0
+  while num <27
+    if num % 3 == 0 && num % 5 == 0
+      "FizzBuzz"
+      elsif num % 5 == 0
+        "Buzz"
+      elsif num % 3 == 0
+        "Fizz"
+      else
+        puts num
+    end
+  end 
+  num += 1
+end
  
-puts fizzbuzz(8)
+puts fizzbuzz
